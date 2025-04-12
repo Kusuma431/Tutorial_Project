cf58bede5ac925556e4d4c227b79b20f4a5449d3
Kusuma431 <bellamkondakusuma431@gmail.com>
2025-04-13 01:39:01 +0530

    webhooks change

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -28,7 +28,7 @@ export const clerkWebhooks = async (req,res)=>{
             }
             case 'user.updated': {
                 const userData ={
-                    email: data.email.address[0].email_address,
+                    email: data.email.addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
 
